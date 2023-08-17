1cac3d2df12d0ffb2a2f9de6a5a3ba2c8158e4b6
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-08-17 10:38:43 +0200

    Add: formatting to files

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const http= require('http'),
-    fs=require('fs'),
-    url=require('url');
+const http = require("http"),
+  fs = require("fs"),
+  url = require("url");
 
+http
+  .createServer((request, response) => {
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
 
-http.createServer((request,response)=>{
-    let addr=request.url,
-     q=url.parse(addr,true),
-     filePath='';
-
-     fs.appendFile('log.txt','URL: ' + addr + '\n Timestamp: ' + new Date() + '\n\n',(err)=>{
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\n Timestamp: " + new Date() + "\n\n",
+      (err) => {
         if (err) {
-            console.log(err);
-        }else{
-            console.log('Added to log.');
+          console.log(err);
+        } else {
+          console.log("Added to log.");
         }
-     });
+      }
+    );
 
-     if(q.pathname.includes('documentation')){
-        filePath=(__dirname + '/documentation.html');
-     }else{
-        filePath='index.html';
-     }
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
 
-     fs.readFile(filePath,(err,data)=>{
-        if(err){
-            throw err;
-        }
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write(data);
-        response.end();
-     });
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080);
 
-}).listen(8080);
-
-console.log('My first Node test server is running on Port 8080.');
+console.log("My first Node test server is running on Port 8080.");
